25aa1b64c8d668f8647510a729e0d9356465904f
Hendrik Ütt <hendrik.utt@coolbet.com>
2024-03-18 14:02:40 +0200

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {CSSProperties, useEffect, useRef, useState} from 'react';
 import './App.css';
 import VideoSrc from './assets/video.mp4';
-import {Player, PlayerRef} from '@remotion/player';
-import {RemotionFrame} from "./RemotionFrame";
+import { Player, PlayerRef } from '@remotion/player';
+import { RemotionFrame } from "./RemotionFrame";
 
 function App() {
     const playerRef = useRef<PlayerRef>(null);
@@ -25,11 +25,17 @@ function App() {
         };
     }, []);
 
+    const videoStyle: CSSProperties = {
+        height: '300px',
+        maxWidth: '100%', // Ensures that video does not exceed the width of its container
+        objectFit: 'cover', // Adjusts the video size to cover the area without losing its aspect ratio
+    };
+
     return (
         <div className="App">
             <header className="App-header">
                 <p>1. Native video tag with static file</p>
-                <video src={VideoSrc} controls style={{ height: '300px' }}></video>
+                <video src={VideoSrc} controls style={videoStyle}></video>
                 <p>2. Remotion player with static file</p>
                 <Player
                     ref={playerRef}
@@ -43,31 +49,30 @@ function App() {
                         videoSrc: VideoSrc
                     }}
                     style={{
-                        height: '300px'
+                        ...videoStyle
                     }}
                 />
 
                 <p>3. Native video tag with blob URL</p>
-                <video src={blobUrl} controls style={{ height: '300px' }}></video>
+                <video src={blobUrl} controls style={videoStyle}></video>
                 <p>4. Remotion player with blob URL</p>
-                {blobUrl !== '' &&
-                    (
-                        <Player
-                            ref={playerRef}
-                            component={RemotionFrame}
-                            durationInFrames={97 * 60}
-                            compositionWidth={1920}
-                            compositionHeight={1080}
-                            fps={60}
-                            controls
-                            inputProps={{
-                                videoSrc: blobUrl
-                            }}
-                            style={{
-                                height: '300px'
-                            }}
-                        />
-                    )}
+                {blobUrl !== '' && (
+                    <Player
+                        ref={playerRef}
+                        component={RemotionFrame}
+                        durationInFrames={97 * 60}
+                        compositionWidth={1920}
+                        compositionHeight={1080}
+                        fps={60}
+                        controls
+                        inputProps={{
+                            videoSrc: blobUrl
+                        }}
+                        style={{
+                            ...videoStyle
+                        }}
+                    />
+                )}
             </header>
         </div>
     );
