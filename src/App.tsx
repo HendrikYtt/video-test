98dfc73061d31c4b142324399d5334556d8008e0
Hendrik Ütt <hendrik.utt@coolbet.com>
2024-03-18 13:54:33 +0200

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
+import VideoSrc from './assets/video.mp4';
+import {Player, PlayerRef} from '@remotion/player';
+import {RemotionFrame} from "./RemotionFrame";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const playerRef = useRef<PlayerRef>(null);
+    const [blobUrl, setBlobUrl] = useState('');
+
+    useEffect(() => {
+        const fetchVideoAsBlobUrl = async () => {
+            const response = await fetch(VideoSrc);
+            const videoBlob = await response.blob();
+            const blobUrl = URL.createObjectURL(videoBlob);
+            setBlobUrl(blobUrl);
+        };
+
+        fetchVideoAsBlobUrl();
+
+        return () => {
+            if (blobUrl) {
+                URL.revokeObjectURL(blobUrl);
+            }
+        };
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>1. Native video tag with static file</p>
+                <video src={VideoSrc} controls style={{ height: '300px' }}></video>
+                <p>2. Remotion player with static file</p>
+                <Player
+                    ref={playerRef}
+                    component={RemotionFrame}
+                    durationInFrames={97*60}
+                    compositionWidth={1920}
+                    compositionHeight={1080}
+                    fps={60}
+                    controls
+                    inputProps={{
+                        videoSrc: VideoSrc
+                    }}
+                    style={{
+                        height: '300px'
+                    }}
+                />
+
+                <p>3. Native video tag with blob URL</p>
+                <video src={blobUrl} controls style={{ height: '300px' }}></video>
+                <p>4. Remotion player with blob URL</p>
+                {blobUrl !== '' &&
+                    (
+                        <Player
+                            ref={playerRef}
+                            component={RemotionFrame}
+                            durationInFrames={97 * 60}
+                            compositionWidth={1920}
+                            compositionHeight={1080}
+                            fps={60}
+                            controls
+                            inputProps={{
+                                videoSrc: blobUrl
+                            }}
+                            style={{
+                                height: '300px'
+                            }}
+                        />
+                    )}
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/RemotionFrame.tsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import {AbsoluteFill, Video} from 'remotion';
+import {z} from 'zod';
+
+export const myCompSchema = z.object({
+	videoSrc: z.string()
+});
+
+export const RemotionFrame: React.FC<z.infer<typeof myCompSchema>> = ({
+	videoSrc
+}) => {
+
+	return (
+		<AbsoluteFill>
+			<Video src={videoSrc} preload="metadata" />
+		</AbsoluteFill>
+	);
+};
+
+RemotionFrame.propTypes = {
+	videoSrc: PropTypes.string.isRequired
+};
